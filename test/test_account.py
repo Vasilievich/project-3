a70b9f9f49740037c3334ed9119fa564c4c87c2b
Caominh <jenny.caominh@abbott.com>
2023-03-01 14:08:38 -0800

    test account creation

--- test/test_account.py
+++ test/test_account.py
@@ -19,7 +19,26 @@ class TestAccount(unittest.TestCase):
         self.driver.maximize_window()
 
     def test_account_creation(self):
-        pass
+        self.sign_in = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/header/nav/div[1]/div/section[2]/ul/li[3]/a').click()
+        time.sleep(3)
+        self.create_account = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[3]/div/div/div/a/span').click()
+        time.sleep(3)
+        self.full_name_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[1]/label/input')
+        self.full_name_field.send_keys("John Smith")
+        self.full_name_field.submit()
+        time.sleep(4)
+        self.email_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[2]/label/input')
+        self.email_field.send_keys("123456abc@gmail.com")
+        self.email_field.submit()
+        time.sleep(4)
+        self.password_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[3]/label/input')
+        self.password_field.send_keys("123456abc")
+        self.password_field.submit()
+        time.sleep(4)
+        self.press_create = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[5]/button/span')
+        self.assertEqual("Create account", self.press_create.text)
+        time.sleep(3)
+
 
     def test_account_login(self):
         pass
