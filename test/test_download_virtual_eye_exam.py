3bf56194831bd7a9b30c42788348a4a7b1d7a186
Caominh <jenny.caominh@abbott.com>
2023-03-02 13:32:58 -0800

    codes clean up, add comments on find store locations

--- test/test_download_virtual_eye_exam.py
+++ test/test_download_virtual_eye_exam.py
@@ -29,10 +29,15 @@ class DownloadVirtualEyeExam(unittest.TestCase):
         testing the download virtual eye exam option
         :return:
         """
+        # click get a prescription button
         get_a_prescription = self.driver.find_element(By.XPATH, "/html/body/div[2]/div/header/nav/div[2]/ul/li[6]/a").click()
         time.sleep(2)
+
+        # click download virtual test
         download_virtual_test = self.driver.find_element(By.XPATH, "/html/body/div[2]/div/main/div/div/div[2]/div/div[2]/div/div[3]/a").click()
         time.sleep(2)
+
+        # check that it has directed to third party download site
         virtual_vision_title = self.driver.find_element(By.XPATH, "/html/body/div[3]/main/div[2]/section[1]/div/div[2]/header/h1")
         self.assertEqual("Virtual Vision Test 12+", virtual_vision_title.text)
         time.sleep(2)
