e5020d9ddc7a7733199e5564af61ff58fc40a46b
Caominh <jenny.caominh@abbott.com>
2023-03-02 13:37:48 -0800

    codes clean up, add comments on test_account

--- test/test_account.py
+++ test/test_account.py
@@ -1,14 +1,13 @@
 import unittest
 import time
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ChromeOptions
 
 opts = ChromeOptions()
 opts.add_argument("--width=800")
 opts.add_argument("--height=600")
-# opts.add_argument("--headless")
+
 
 class TestAccount(unittest.TestCase):
     """
@@ -28,21 +27,31 @@ class TestAccount(unittest.TestCase):
         """
         test creating account function
         """
-        # click sign in
+        # navigate to sign in page
         self.driver.find_element(By.XPATH, '/html/body/div[2]/div/header/nav/div[1]/div/section[2]/ul/li[3]/a').click()
         time.sleep(2)
+
         # create account
         self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[3]/div/div/div/a/span').click()
         time.sleep(2)
+
+        # locate name input field
         full_name_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[1]/label/input')
+        # input name
         full_name_field.send_keys("John Smith")
         time.sleep(4)
+        # locate email address input field
         email_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[2]/label/input')
+        # input email address
         email_field.send_keys("1234567@gmail.com")
         time.sleep(4)
+        # locate password input field
         password_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[3]/label/input')
+        # input password
         password_field.send_keys("1234567890")
         time.sleep(4)
+
+        # check that account has been created
         press_create = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[5]/button/span')
         self.assertEqual("Create account", press_create.text)
         time.sleep(3)
@@ -52,18 +61,24 @@ class TestAccount(unittest.TestCase):
         """
         test sign in function
         """
-        # click sign in page
+        # navigate to sign in page
         self.driver.find_element(By.XPATH, '/html/body/div[2]/div/header/nav/div[1]/div/section[2]/ul/li[3]/a').click()
         time.sleep(3)
+        # locate email address input field
         email_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[1]/label/input')
+        # input email address
         email_field.send_keys("1234567@gmail.com")
         time.sleep(4)
+        # locate password input field
         password_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[2]/label/input')
+        # input password
         password_field.send_keys("1234567890")
         time.sleep(4)
         # click sign in button
         self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[4]/button/span').click()
         time.sleep(4)
+
+        # confirm sign in is successful
         press_sign_out = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/main/div/div/p/a')
         self.assertEqual("Sign out", press_sign_out.text)
         time.sleep(3)
