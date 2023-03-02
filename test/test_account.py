e7ad1f648af11a5c0195349663b6e21eea886b81
Caominh <jenny.caominh@abbott.com>
2023-03-02 12:48:12 -0800

    update code in test_account_creation

--- test/test_account.py
+++ test/test_account.py
@@ -25,21 +25,23 @@ class TestAccount(unittest.TestCase):
         self.driver.maximize_window()
 
     def test_account_creation(self):
+        # click sign in
         self.driver.find_element(By.XPATH, '/html/body/div[2]/div/header/nav/div[1]/div/section[2]/ul/li[3]/a').click()
         time.sleep(2)
-        self.create_account = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[3]/div/div/div/a/span').click()
+        # create account
+        self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[3]/div/div/div/a/span').click()
         time.sleep(2)
-        self.full_name_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[1]/label/input')
-        self.full_name_field.send_keys("John Smith")
+        full_name_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[1]/label/input')
+        full_name_field.send_keys("John Smith")
         time.sleep(4)
-        self.email_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[2]/label/input')
-        self.email_field.send_keys("1234567@gmail.com")
+        email_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[2]/label/input')
+        email_field.send_keys("1234567@gmail.com")
         time.sleep(4)
-        self.password_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[3]/label/input')
-        self.password_field.send_keys("1234567890")
+        password_field = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[3]/label/input')
+        password_field.send_keys("1234567890")
         time.sleep(4)
-        self.press_create = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[5]/button/span')
-        self.assertEqual("Create account", self.press_create.text)
+        press_create = self.driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div[2]/div/div[2]/div/div[1]/div/div/div[2]/div/form/div[5]/button/span')
+        self.assertEqual("Create account", press_create.text)
         time.sleep(3)
 
 
